341fbcd4143153dec129a201bfd406c9308be8b9
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-12-03 20:56:22 +0700

    menambahkan coolision shape dan y short

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,12 +1,40 @@
 extends CharacterBody2D
 
+@onready var animasi_player = $AnimatedSprite2D
 
-const SPEED = 300
+const SPEED = 2000
+var last_direction = Vector2.ZERO
 
 func _physics_process(delta):
 	move_player(delta)
+	update_animation(delta)
 	
-func move_player(_delta):
+func move_player(delta):
 	var direction = Input.get_vector('ui_left','ui_right','ui_up','ui_down')
-	velocity = direction * SPEED
+	velocity = direction * SPEED * delta
+	move_and_slide()
+	
+func update_animation(_delta):
+	var direction = Input.get_vector('ui_left','ui_right','ui_up','ui_down')
+	
+	if direction != Vector2.ZERO:
+		last_direction = direction
+		
+	if direction.x != 0 :
+		animasi_player.play('walk_side')
+	elif direction.y < 0 :
+		animasi_player.play("walk_up")
+	elif direction.y > 0 :
+		animasi_player.play("walk_down")
+	else:
+		
+		if last_direction.x != 0 :
+			animasi_player.play("iddle_side")
+		elif last_direction.y < 0 :
+			animasi_player.play("iddle_up")
+		elif last_direction.y > 0 :
+			animasi_player.play("iddle_down")
+			
+	animasi_player.flip_h = last_direction.x < 0
+	
 	move_and_slide()
