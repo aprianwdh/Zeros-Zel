ebd9defd859bb8dc7fd01f5d4d26a93c84bcce4d
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-12-05 20:46:13 +0700

    membuat combat system

--- Scripts/Enemy/slime.gd
+++ Scripts/Enemy/slime.gd
@@ -4,21 +4,30 @@ extends CharacterBody2D
 var chase_player = false
 const SPEED = 100
 var player
+var health = 100
+var death = false
+var target
 @onready var animated_sprite_2d = $AnimatedSprite2D
+var last_position
+var waktu_respawn : int = 2
+
+func _ready():
+	last_position = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if chase_player:
-		position += (player.position - position) / SPEED
-		
-		animated_sprite_2d.play('walk')
-		
-		if (player.position.x - position.x) < 0:
-			animated_sprite_2d.flip_h = true
-		else :
-			animated_sprite_2d.flip_h = false
-	else:
-		animated_sprite_2d.play("iddle")
+	if death == false:
+		if chase_player:
+			position += (player.position - position) / SPEED
+			
+			animated_sprite_2d.play('walk')
+			
+			if (player.position.x - position.x) < 0:
+				animated_sprite_2d.flip_h = true
+			else :
+				animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.play("iddle")
 
 func _on_chase_area_body_entered(body):
 	if body.is_in_group("Player"):
@@ -30,3 +39,49 @@ func _on_chase_area_body_exited(body):
 	if body.is_in_group("Player"):
 		player = null
 		chase_player = false
+
+func attacked():
+	health -= 20
+	print(health)
+	$AnimatedSprite2D.modulate = Color(1, 0.5, 0.5)  # Flash merah
+	await get_tree().create_timer(0.1).timeout  # Tunggu 0.1 detik
+	$AnimatedSprite2D.modulate = Color(1, 1, 1)  # Kembali normal
+	print(health)
+	if health <= 0:
+		death = true
+		die()
+		
+func die():
+	if death == true :
+		$AnimatedSprite2D.play("mati")
+		await  animated_sprite_2d.animation_finished
+		$CollisionShape2D2.disabled = true
+		hide()
+		await get_tree().create_timer(waktu_respawn).timeout
+		respawn()
+		
+func respawn():
+	global_position = last_position  # Kembalikan ke posisi awal
+	health = 100  # Pulihkan health
+	death = false  # Status hidup kembali
+	$CollisionShape2D2.disabled = false  # Aktifkan kembali bentuk kolisi
+	show()  # Tampilkan node utama
+	$AnimatedSprite2D.play("iddle")  # Mulai animasi diam
+
+func _on_attack_area_body_entered(body):
+	if body.is_in_group("Player") and death == false:
+		target = body
+		$attack_timer.start()
+
+
+
+func _on_attack_timer_timeout():
+		target.attacked_player()
+
+
+
+
+func _on_attack_area_body_exited(body):
+	if body.is_in_group("Player"):
+		target = null
+		$attack_timer.stop()
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -6,14 +6,17 @@ const SPEED = 2000
 var last_direction = Vector2.ZERO
 @export var can_attck = false
 
+var health_player = 100
+var death_player = false
 
 func _ready():
 	animasi_player.play("iddle_down")
 	last_direction.y = 1
 func _physics_process(delta):
-	move_player(delta)
-	update_animation(delta)
-	animasi_attack()
+	if death_player == false:
+		move_player(delta)
+		update_animation(delta)
+		animasi_attack()
 	
 func move_player(delta):
 	var direction = Input.get_vector('left','right','up','down')
@@ -70,5 +73,31 @@ func animasi_attack():
 					animasi_player.play("atack_down")
 					
 			$AnimatedSprite2D.flip_h = last_direction.x < 0
+			if last_direction.x < 0 :
+				$attack_area/CollisionShape2D.position = Vector2(-11,4)
+			else:
+				$attack_area/CollisionShape2D.position = Vector2(11,4)
 	
-	move_and_slide()
+	#move_and_slide()
+
+
+func _on_attack_area_body_entered(body):
+	if body.has_method('attacked'):
+		body.attacked()
+		
+func attacked_player():
+	if death_player == false:
+		health_player -= 20
+		$AnimatedSprite2D.modulate = Color(1, 0.5, 0.5)  # Flash merah
+		await get_tree().create_timer(0.1).timeout  # Tunggu 0.1 detik
+		$AnimatedSprite2D.modulate = Color(1, 1, 1)  # Kembali normal
+
+		if health_player <= 0:
+			death_player = true
+			die_player()
+		
+func die_player():
+	if death_player == true :
+		animasi_player.play("die")
+		await animasi_player.animation_finished
+		#queue_free()
